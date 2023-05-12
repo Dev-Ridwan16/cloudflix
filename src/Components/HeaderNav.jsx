4c7aafbf56dde9d925986fe75feb30e800c83f07
Thrive Coder <thrivetech793@gmail.com>
2023-05-12 08:29:05 -0700

    remove the default opacity in the mobile nav

--- src/Components/HeaderNav.jsx
+++ src/Components/HeaderNav.jsx
@@ -24,7 +24,7 @@ function HeaderNav() {
           <i className="ri-user-3-line"></i>
         </div>
         <div>
-          <nav className={`transition-all duration-500 ease-in-out ${nav ? 'opacity-0 w-0' : 'opacity-100 w-full'} opacity-100 flex flex-col h-screen fixed ${handleTheme ? 'bg-[#070101] transition-all duration-1000 ease-in-out' : 'bg-[#ffffff]'}`}>
+          <nav className={`${nav ? 'opacity-0 w-0' : 'opacity-100 w-auto'} transition-all duration-500 ease-in-out flex flex-col h-screen fixed ${handleTheme ? 'bg-[#070101] transition-all duration-1000 ease-in-out' : 'bg-[#ffffff]'}`}>
 
                 {mobileNavLinks.map((mobileNavLink, index) => {
                 return (
